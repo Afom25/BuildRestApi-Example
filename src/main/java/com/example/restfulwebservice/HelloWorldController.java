89ac3705d2fac3e38ae54097f86681bb2118b452
hidrighebretinsae <afomafom111@gmail.com>
2022-09-13 21:28:55 -0500

    this commit can creates the the files for all the application

---
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.example.restfulwebservice;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//REST API
+@RestController
+
+public class HelloWorldController {
+
+    @RequestMapping(method = RequestMethod.GET,path = "/hello")
+    public String helloworld(){
+        return "Hello world HIDRI";
+    }
+}

eed6f8fdeb2a1eedc180a7d69241f61a02351590
hidrighebretinsae <afomafom111@gmail.com>
2022-09-13 23:50:48 -0500

    this commis t creates the User class

--- src/main/java/com/example/restfulwebservice/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservice;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //REST API
@@ -17,5 +18,9 @@ public class HelloWorldController {
     @GetMapping(path="/hello-bean")
     public HelloworldBean helloworldBean(){
         return new HelloworldBean(" HIDRI-BEAN");
+    }
+    @GetMapping(path="/hello-bean/path-variable/{name}")
+    public HelloworldBean helloworlPathVariable(@PathVariable String name){
+        return new HelloworldBean(String.format("heloo,%s",name));
     }
 }
